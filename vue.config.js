1519502f24ecba69df5f0f3d7d973abf4b675ea6
Tommy Ng <tommynhm@gmail.com>
2021-01-08 16:37:59 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -12,12 +12,6 @@ module.exports = {
     }
   },
   pwa: {
-    themeColor: "#42b983",
-    msTileColor: "#42b983",
-    appleMobileWebAppCache: "yes",
-    manifestOptions: {
-      background_color: "#42b983"
-    },
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       swSrc: 'src/service-worker.js',
