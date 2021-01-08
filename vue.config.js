bc2c836e42b539a6295eba62c7079c5f937aa209
Tommy Ng <tommynhm@gmail.com>
2021-01-08 15:24:17 +0800

    Test PWA

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,14 @@ module.exports = {
       }
     }
   },
+  pwa: {
+    themeColor: "#42b983",
+    msTileColor: "#42b983",
+    appleMobileWebAppCache: "yes",
+    manifestOptions: {
+      background_color: "#42b983"
+    }
+  },
   configureWebpack: () => {
     if (process.env.NODE_ENV !== 'production') return;
     return {
