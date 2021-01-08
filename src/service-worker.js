cb24d57041e1e1e5781c7777eb15dfb32c111d12
Tommy Ng <tommynhm@gmail.com>
2021-01-08 16:17:40 +0800

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,11 @@
-console.log('I am a Service Worker!');
+self.addEventListener('install', (event) => {
+  console.log('Inside the install handler:', event);
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Inside the activate handler:', event);
+});
+
+self.addEventListener(fetch, (event) => {
+  console.log('Inside the fetch handler:', event);
+});
