89d92ecf658411e5be816118081f95459e782edf
Tommy Ng <tommynhm@gmail.com>
2021-01-08 17:04:21 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import './registerServiceWorker'
 import router from '@/router/router'
 import App from './App.vue'
+import "alertifyjs/build/css/alertify.css"
+import "alertifyjs/build/css/themes/semantic.css"
+import "alertifyjs/build/alertify"
 import "./assets/styles/index.scss"
 
 const app = createApp(App)
